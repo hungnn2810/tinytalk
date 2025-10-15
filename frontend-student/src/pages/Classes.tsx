3398dc2e3221e570e9f159aafbcd3d521c07262a
hungnn <hungnn2810@gmail.com>
2025-10-15 18:21:10 +0700

    up

--- frontend-student/src/pages/Classes.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from '@chakra-ui/react';
-
-export default function Classes() {
-    return <Heading>Classes Page</Heading>;
-}
--- frontend-student/src/pages/Profile.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from '@chakra-ui/react';
-
-export default function Profile() {
-    return <Heading>Profile Page</Heading>;
-}
